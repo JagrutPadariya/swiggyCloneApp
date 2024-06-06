7874d5a57c26e1086ccaaf09dd18ae439989c9e3
jagrut <jagrutpadariya7@gmail.com>
2024-06-06 13:23:44 +0530

    create swiggyCloneApp

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,69 @@
+import User from "../models/User";
+import { NodeMailer } from "../utils/NodeMailer";
+import { Utils } from "../utils/Utils";
+
+export class UserController {
+  static async signup(req, res, next) {
+    console.log("req: ", req);
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const password = req.body.password;
+    const name = req.body.name;
+    const type = req.body.type;
+    const status = req.body.status;
+    const verification_token = Utils.generateVerificationToken();
+
+    const data = {
+      email,
+      verification_token,
+      verification_token_time: Date.now() + new Utils().MAX_TOKEN_TIME,
+      phone,
+      password,
+      name,
+      type,
+      status,
+    };
+
+    try {
+      let user = await new User(data).save();
+      // send email to user for verification
+      await NodeMailer.sendMail({
+        to: [email],
+        subject: "test",
+        html: `<h1>Your Otp is ${verification_token}</h1>`,
+      });
+      res.send(user);
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  static async verify(req, res, next) {
+    const verification_token = req.body.verification_token;
+    const email = req.body.email;
+    try {
+      const user = await User.findOneAndUpdate(
+        {
+          email: email,
+          verification_token: verification_token,
+          verification_token_time: { $gt: Date.now() },
+        },
+        {
+          email_verified: true,
+        },
+        {
+          new: true,
+        }
+      );
+      if (user) {
+        res.send(user);
+      } else {
+        throw new Error(
+          "Email Verification Token Is Expired. Please try again..."
+        );
+      }
+    } catch (e) {
+      next(e);
+    }
+  }
+}
---
+++ src/server.ts
@@ -0,0 +1,64 @@
+import * as express from "express";
+import * as mongoose from "mongoose";
+import { getEnvironmentVariables } from "./environments/environment";
+import UserRouter from "./routers/UserRouter";
+import * as bodyParser from "body-parser";
+import * as cors from "cors";
+
+export class Server {
+  public app: express.Application = express();
+
+  constructor() {
+    this.setConfigs();
+    this.setRoutes();
+    this.error404Handler();
+    this.handleErrors();
+  }
+
+  setConfigs() {
+    this.connectMongoDB();
+    this.configureBodyParser();
+    this.allowCors();
+  }
+
+  connectMongoDB() {
+    mongoose.connect(getEnvironmentVariables().db_uri).then(() => {
+      console.log("Connected to mongodb.");
+    });
+  }
+
+  configureBodyParser() {
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
+  }
+
+  allowCors() {
+    this.app.use(cors());
+  }
+
+  setRoutes() {
+    this.app.use("/api/user", UserRouter);
+  }
+
+  error404Handler() {
+    this.app.use((req, res) => {
+      res.status(404).json({
+        messages: "Not found",
+        status_code: 404,
+      });
+    });
+  }
+
+  handleErrors() {
+    this.app.use((error, req, res, next) => {
+      const errorStatus = req.errorStatus || 500;
+      res.status(errorStatus).json({
+        messages: error.message || "Something went wrong. Please try again!",
+        status_code: errorStatus,
+      });
+    });
+  }
+}
